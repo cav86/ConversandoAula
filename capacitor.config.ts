1fee9899cdae2c0295d3c50c1cd3765cb50352c5
cav86 <cav.utn@gmail.com>
2025-05-27 03:18:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ConversandoAula',
+  webDir: 'www'
+};
+
+export default config;
